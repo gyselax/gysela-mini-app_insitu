397f6a726e3d8ca81bf21b6623a8ab68ed3ae324
Earl TShirt <earltshirt@macnifique-pro.home>
2026-09-23 09:51:52 +0200

    Reorganize compression app folder structure

--- apps/io/env-miniapp-io.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BASE_DIR="$(cd $SCRIPT_DIR/../.. && pwd)"
-
-source $BASE_DIR/src/external/gyselalibxx/toolchains/cpu.spack.gyselalibxx_env/environment.sh
-
-. $BASE_DIR/venv/bin/activate
